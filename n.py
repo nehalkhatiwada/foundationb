172557f78d15a77df90c4f115695be9e3ee7e5ce
Nehal khatiwada <nehalkhatiwada2542@gmail.com>
2022-09-12 10:08:38 +0545

    hfjhe

---
+++ n.py
@@ -0,0 +1,14 @@
+# This is a single line comment 
+name=input("Enter your name")
+age=input ("Enter ypur age")
+print(f"Hi, your name is (name) and your (age) ")
+
+
+# conditional statement
+# if block only
+
+
+print("Code started .....")
+if age >18;
+    print(f"You can play this game because your age")
+print("Code ended .....")
